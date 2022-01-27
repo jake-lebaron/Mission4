85243eef57989b83964808000c91622e309ecd94
jake-lebaron <jlebaron597@gmail.com>
2022-01-27 16:50:42 -0700

    Add project files.

---
+++ Mission4/Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private MovieInputContext blahContext { get; set; }
+
+        //Constructor
+        public HomeController(ILogger<HomeController> logger, MovieInputContext someName)
+        {
+            _logger = logger;
+            blahContext = someName;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult MovieInput ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult MovieInput(MovieResponse asdf)
+        {
+            blahContext.Add(asdf);
+            blahContext.SaveChanges();
+
+            return View("Index");
+        }
+
+        public IActionResult Podcast()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
